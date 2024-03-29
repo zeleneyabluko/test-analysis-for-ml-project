b16473ee762170807474bf0c42270cbdba3bb9a4
Svitlana Dmytrenko <zeleneyabluko@gmail.com>
2024-03-29 22:23:26 +0200

    updated definition of labels

--- src/buildmatrix.py
+++ src/buildmatrix.py
@@ -42,7 +42,7 @@ def getpredictedvalues(model_files):
 
 
 def buildmatrix(marked_values, predicted_values):
-    labels = np.unique(marked_values)
+    labels = labels = np.unique(np.concatenate((marked_values, predicted_values)))
     matrix = confusion_matrix(marked_values, predicted_values, labels=labels)
     matrix_df = pd.DataFrame(matrix, index=labels, columns=labels)
     return matrix_df
