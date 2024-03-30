9e88f403588e7f5bc835b0ee2cb91e08658b6afa
Svitlana Dmytrenko <zeleneyabluko@gmail.com>
2024-03-30 13:52:50 +0200

    minor fixes

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 from buildmatrix import getlistoffiles, getmarkedvalues, getpredictedvalues, buildmulticlassmatrix, definelabels,buildbinarymatrices
 
 # Your model string
-model = "model1"
+model = "model3"
 
 # Call the function with the model string
 files = getlistoffiles(model)
@@ -12,5 +12,9 @@ marked_values = getmarkedvalues(files)
 predicted_values=getpredictedvalues(files)
 labels = definelabels(marked_values, predicted_values)
 cm = buildmulticlassmatrix(marked_values, predicted_values, labels)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
 print(cm)
 buildbinarymatrices(marked_values, predicted_values, labels)
